b80e2bc286e25e8ee463e0a59066d3939987b062
Kid Omar Costelo <kidomar.costelo@cit.edu>
2024-04-07 19:57:44 +0800

    Update dependencies and configuration for Truffle and Hardhat

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -1,7 +1,7 @@
-import CollectionConfigInterface from '../lib/CollectionConfigInterface';
-import * as Networks from '../lib/Networks';
-import * as Marketplaces from '../lib/Marketplaces';
-import whitelistAddresses from './whitelist.json';
+import CollectionConfigInterface from "../lib/CollectionConfigInterface";
+import * as Networks from "../lib/Networks";
+import * as Marketplaces from "../lib/Marketplaces";
+import whitelistAddresses from "./whitelist.json";
 
 const CollectionConfig: CollectionConfigInterface = {
   testnet: Networks.ethereumTestnet,
@@ -9,25 +9,26 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
-  hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  contractName: "ElevineNFT",
+  tokenName: "Elevine", // mao ni makita sa website title
+  tokenSymbol: "ELVN",
+  hiddenMetadataUri:
+    "ipfs://QmWPriJGQN3QrC3nLRShARrV2Jrq2st66wn3S8sZMP19mD/hidden.json",
+  maxSupply: 10,
   whitelistSale: {
-    price: 0.05,
-    maxMintAmountPerTx: 1,
+    price: 0.015,
+    maxMintAmountPerTx: 5,
   },
   preSale: {
-    price: 0.07,
+    price: 0.015,
     maxMintAmountPerTx: 2,
   },
   publicSale: {
-    price: 0.09,
+    price: 0.02,
     maxMintAmountPerTx: 5,
   },
-  contractAddress: null,
-  marketplaceIdentifier: 'my-nft-token',
+  contractAddress: "0x0dbb917a1AA94eAe48F66c17F6117ec7B902F362",
+  marketplaceIdentifier: "ELEVINE-NI-CHUY",
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
 };
